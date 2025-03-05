e9276fa5b704d36f1541292d28e516454b33f566
Oanh <kimoanh16092005@gmail.com>
2025-03-05 23:49:58 +0700

    labwork4ex3

--- labwork4/c3.c
+++ labwork4/c3.c
@@ -13,6 +13,26 @@ Exercise 5:
 Write two C functions to calculate xn , n and x are input from the keyboard by the normal function and recursive function.
 Note: All the functions should be defined in two separated files: *.h and *.c*/
 
+#include <stdio.h>
+
+float f(float x){
+    float e;
+    e=2.5*x;
+    return e;
+}
+
+float g(float x,float y){
+    return x*y;
+}
+
+int main(){
+    float x,y;
+    printf("enter the number x and y:");
+    scanf("%f %f",&x,&y);
+    printf(" f(x) =%.3f \n g(x,y)=%.3f\n",f(x),g(x,y));
+    return 0;
+}
+
 
 
 
